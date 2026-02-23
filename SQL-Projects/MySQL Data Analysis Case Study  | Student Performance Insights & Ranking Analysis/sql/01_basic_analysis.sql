6d09f4fd97f54602db1df3d425dca711f3924223
Athulnath <61876886+athulnaths@users.noreply.github.com>
2026-02-23 23:03:54 +0530

    Update 01_basic_analysis.sql

--- SQL-Projects/MySQL Data Analysis Case Study  | Student Performance Insights & Ranking Analysis/sql/01_basic_analysis.sql
+++ SQL-Projects/MySQL Data Analysis Case Study  | Student Performance Insights & Ranking Analysis/sql/01_basic_analysis.sql
@@ -1,1 +1,26 @@
+-- 1. Total number of students
+select count(*) as total_students
+from studentperformancefactors;
 
+--  2. Find the average Exam Score of all students.
+SELECT 
+    AVG(exam_score) AS avg_exam_score
+FROM
+    studentperformancefactors;
+
+-- 3. Find the average Attendance for each Gender.
+SELECT 
+    gender,
+    AVG(attendance) AS avg_attendance,
+    AVG(Exam_Score) AS avg_score
+FROM
+    studentperformancefactors
+GROUP BY gender;
+
+-- 4. Students who scored above 80
+SELECT 
+    *
+FROM
+    studentperformancefactors
+WHERE
+    Exam_Score > 80;
