50a6cf14661e2d67a1f26e124b4d307eee9e13b4
Athulnath <61876886+athulnaths@users.noreply.github.com>
2026-02-23 23:12:07 +0530

    Update 02_intermediate_analysis.sql

--- SQL-Projects/MySQL Data Analysis Case Study  | Student Performance Insights & Ranking Analysis/sql/02_intermediate_analysis.sql
+++ SQL-Projects/MySQL Data Analysis Case Study  | Student Performance Insights & Ranking Analysis/sql/02_intermediate_analysis.sql
@@ -1,1 +1,45 @@
+-- 1. Average score by parental involvement
+SELECT 
+    Parental_Involvement, ROUND(AVG(Exam_Score), 2) AS avg_score
+FROM
+    studentperformancefactors
+GROUP BY Parental_Involvement;
+
+-- 2. Calculate the average Exam_Score for students with and without Tutoring_Sessions.
+SELECT 
+    Tutoring_Sessions,
+    COUNT(*) AS Total_students,
+    ROUND(AVG(Exam_Score), 2) AS avg_exam_score
+FROM
+    studentperformancefactors
+GROUP BY Tutoring_Sessions
+ORDER BY Tutoring_Sessions;
+
+-- 3. Display students whose Exam_Score is above the overall average score.
+SELECT 
+    Gender, School_Type, Exam_Score
+FROM
+    studentperformancefactors
+WHERE
+    Exam_Score > (SELECT 
+            AVG(Exam_Score)
+        FROM
+            studentperformancefactors)
+ORDER BY Exam_Score;
+
+-- 4. Use a CASE statement to classify Exam Score as:
+-- High (>= 75)
+-- Medium (60–74)
+-- Low (< 60)
+-- and count students in each category.
+select 
+case
+when Exam_Score >=75 then 'High'
+when Exam_Score between 60 and 74 then 'Medium'
+else 'Low'
+end as Score_category,
+count(*) as total_students
+from studentperformancefactors
+group by Score_category
+order by total_students desc;
 
